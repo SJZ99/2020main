388d0cd0e7afa11dd0df91fa4d4ace972467d09b
nike951847 <sos7mi@gmail.com>
2020-03-08 14:10:04 +0800

    path add

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -39,7 +39,7 @@ public final class Constants {
         public static final int falconCPR =2048; 
         public static final int maxspeed=10000,maxacc=5000;
         public static final double enoderunit = 2048/(0.1524*Math.PI);
-        public static final String LeftInitToCP = "C:\Users\ASUS\Desktop\2020robot2.0-master\src\main\deploy\output\LeftInitToCP.wpilib.json";
+        public static final String LeftInitToCP = "output/LeftInitToCP.wpilib.json";
         public static final String MidnitToCP = "output/path.wpilib.json";
         //Attributes
         public static final double wheelDiamete = 0.1524;
