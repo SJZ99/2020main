cdf7863c917e0dbecbb5fae1eda830dd1e4b7a1c
SJZ99 <9987jdvrb@gmail.com>
2020-06-28 11:06:16 +0800

    trajectory nullpointexception

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -43,8 +43,7 @@ public class Robot extends TimedRobot {
   }
   /**
    * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
+   * diagnostics that you wa
    * <p>This runs after the mode specific periodic functions, but before
    * LiveWindow and SmartDashboard integrated updating.
    */
